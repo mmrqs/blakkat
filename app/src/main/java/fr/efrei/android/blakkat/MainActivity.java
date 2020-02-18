fb8ab05a417620a2a603fe957c46ffb9e2f74d8d
PrideTheDaemon <hellangel@live.fr>
2020-02-18 11:53:20 +0100

    Created Android project

---
+++ app/src/main/java/fr/efrei/android/blakkat/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.efrei.android.blakkat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

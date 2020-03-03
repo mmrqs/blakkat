7853bfc111f64cc3e68329232ee64fb0f2e92a78
PrideTheDaemon <hellangel@live.fr>
2020-03-03 22:34:04 +0100

    Reverted SearchActivity for the sake of my survival (major technical KO threat)

--- app/src/main/java/fr/efrei/android/blakkat/SearchActivity.java
+++ app/src/main/java/fr/efrei/android/blakkat/SearchActivity.java
@@ -1,18 +1,30 @@
 package fr.efrei.android.blakkat;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.SearchView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
+import fr.efrei.android.blakkat.consuming.providers.ProviderHelper;
 import fr.efrei.android.blakkat.model.IMedia;
+import fr.efrei.android.blakkat.model.Manga;
 import fr.efrei.android.blakkat.model.Show;
 import fr.efrei.android.blakkat.view.Adapters.CardAdapter;
 import retrofit2.Call;
@@ -20,6 +32,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class SearchActivity extends AppCompatActivity {
+
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -42,10 +55,10 @@ public class SearchActivity extends AppCompatActivity {
             public boolean onQueryTextSubmit(String query) {
                 String t = r.getQuery().toString();
 
-//                ProviderHelper providerHelper = new ProviderHelper();
+                ProviderHelper providerHelper = new ProviderHelper();
 
                 List<IMedia> results = new ArrayList<>();
-/*
+
                 providerHelper.getGeneralProvider().getShowProvider()
                         .searchFor5Results(t,5).enqueue(new Callback<List<Show>>() {
                     @Override
@@ -55,12 +68,12 @@ public class SearchActivity extends AppCompatActivity {
                         mAdapter = new CardAdapter(results);
                         recyclerView.setAdapter(mAdapter);
                     }
+
                     @Override
                     public void onFailure(Call<List<Show>> call, Throwable t) {
                         Log.e("", t.toString());
                     }
-                });*/
-
+                });
                 return false;
             }
 

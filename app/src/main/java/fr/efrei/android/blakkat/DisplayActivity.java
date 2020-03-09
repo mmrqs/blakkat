2e48939907a53cdf47588be51ceb140fe92b9472
Mélanie <marquesmelanie5@gmail.com>
2020-03-09 10:04:30 +0100

    comiiiiiiiit

--- app/src/main/java/fr/efrei/android/blakkat/DisplayActivity.java
+++ app/src/main/java/fr/efrei/android/blakkat/DisplayActivity.java
@@ -1,6 +1,8 @@
 package fr.efrei.android.blakkat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +12,8 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
@@ -17,6 +21,8 @@ import java.util.Objects;
 import fr.efrei.android.blakkat.model.Media;
 import fr.efrei.android.blakkat.model.MediaRecord;
 import fr.efrei.android.blakkat.model.Show;
+import fr.efrei.android.blakkat.view.Adapters.CardAdapter;
+import fr.efrei.android.blakkat.view.Adapters.SeriesAdvancementAdapter;
 
 public class DisplayActivity extends AppCompatActivity {
     private TextView titleDisplay;
@@ -26,6 +32,10 @@ public class DisplayActivity extends AppCompatActivity {
     private Button returnButton;
     private TextView genre;
     private Button viewedToggleButton;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,13 +74,36 @@ public class DisplayActivity extends AppCompatActivity {
         this.changeToggleViewedButtonContents(MediaRecord
                 .exists(result.getId(), result.getProviderHint()));
 
+        //TODO : delete this
         if(result.getProviderHint().equals("Show")) {
             System.out.println((HashMap) result.getSeasons());
         }
-        System.out.println("coucou");
         if(result.getProviderHint().equals("Anime")) {
             System.out.println((Integer) result.getSeasons());
         }
+
+        recyclerView = findViewById(R.id.RecyclerView_ActivityDisplay);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Seasons cards :
+        switch (result.getProviderHint()) {
+            case "Show" :
+                mAdapter = new SeriesAdvancementAdapter(seasonsFormatter(result.getSeasons()),
+                        DisplayActivity.this);
+                break;
+            case "Anime" :
+            case "Manga" :
+                mAdapter = new SeriesAdvancementAdapter(mangaAnimeFormatter(result.getSeasons()),
+                        DisplayActivity.this);
+                break;
+            default :
+                break;
+        }
+
+        recyclerView.setAdapter(mAdapter);
 
 
         viewedToggleButton.setOnClickListener(view -> {
@@ -88,5 +121,23 @@ public class DisplayActivity extends AppCompatActivity {
 
     void changeToggleViewedButtonContents(MediaRecord mr) {
         viewedToggleButton.setText(mr == null ? R.string.notviewed : R.string.viewed);
+    }
+
+    public ArrayList<String> seasonsFormatter(HashMap<Integer, Integer> h) {
+        ArrayList<String> a = new ArrayList<>();
+        for (int i : h.keySet()) {
+            for (int j = 1; j <= h.get(i) ; j++) {
+                a.add("Saison : " + i + " Episode " + j);
+            }
+        }
+        return a;
+    }
+
+    public ArrayList<String> mangaAnimeFormatter ( int nbVolumes ) {
+        ArrayList<String> a = new ArrayList<>();
+        for (int i = 1; i <= nbVolumes; i++) {
+            a.add("Volume : " + i);
+        }
+        return a;
     }
 }

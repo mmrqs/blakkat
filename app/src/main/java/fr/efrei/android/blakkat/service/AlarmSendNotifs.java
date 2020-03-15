bb6ccaf458c2b6e5f36b021d842962dee6e22ad7
Mélanie <marquesmelanie5@gmail.com>
2020-03-15 22:50:59 +0100

    go to blakkat with notification

--- app/src/main/java/fr/efrei/android/blakkat/service/AlarmSendNotifs.java
+++ app/src/main/java/fr/efrei/android/blakkat/service/AlarmSendNotifs.java
@@ -11,6 +11,7 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import fr.efrei.android.blakkat.R;
+import fr.efrei.android.blakkat.ui.activities.LoginActivity;
 
 public class AlarmSendNotifs extends BroadcastReceiver {
     private static final int NOTIFICATION_ID = 666;
@@ -18,11 +19,18 @@ public class AlarmSendNotifs extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        Intent goToBlakkat = new Intent(context, LoginActivity.class);
+        goToBlakkat.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, goToBlakkat, 0);
+
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.drawable.logo_blakkat_dark)
                 .setContentTitle("Blakkat")
                 .setContentText("Hey! De nouveaux médias vu? Viens les enregistrer :)")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
@@ -35,6 +43,6 @@ public class AlarmSendNotifs extends BroadcastReceiver {
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context, AlarmSendNotifs.class);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
-        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 * 60, pi);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 * 1, pi);
     }
 }

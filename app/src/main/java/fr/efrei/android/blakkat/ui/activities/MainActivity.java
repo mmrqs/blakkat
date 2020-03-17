1a420555988821521613370cdf379bf5025b1490
Mélanie <marquesmelanie5@gmail.com>
2020-03-17 10:01:08 +0100

    return

--- app/src/main/java/fr/efrei/android/blakkat/ui/activities/MainActivity.java
+++ app/src/main/java/fr/efrei/android/blakkat/ui/activities/MainActivity.java
@@ -75,6 +75,9 @@ public class MainActivity extends AppCompatActivity
             case R.id.action_settings:
                 startActivity(new Intent(this, SettingsActivity.class));
                 return true;
+            case android.R.id.home:
+                onBackPressed();
+                return true;
             default:
                 Toaster.burn(this, "Error");
                 return false;
